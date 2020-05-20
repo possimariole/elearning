093136c5abc2079b2e3832a61f2bfab62948a49d
samuel <s60programmeomega@gmail.com>
2020-05-20 22:31:54 +0100

    Correction des bogues

--- src/Controller/AnneeAcademiqueController.php
+++ src/Controller/AnneeAcademiqueController.php
@@ -10,12 +10,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @Route("/anneeacademique")
  */
 class AnneeAcademiqueController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security) {
+        $this->security = $security;
+    }
+
     /**
      * @Route("/", name="annee_academique_index", methods={"GET"})
      */
